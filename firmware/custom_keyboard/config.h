bb580e839ddcdb01596582cbc050e98d65bbcabc
Aaron Shappell <ashapp@uw.edu>
2018-11-26 22:38:49 -0800

    Remove backlighting for now

--- firmware/custom_keyboard/config.h
+++ firmware/custom_keyboard/config.h
@@ -48,10 +48,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
 #define DIODE_DIRECTION COL2ROW
 
-#define BACKLIGHT_PIN B7
-#define BACKLIGHT_BREATHING
-#define BACKLIGHT_LEVELS 3
-#define BREATHING_PERIOD 6
+// #define BACKLIGHT_PIN B7
+// #define BACKLIGHT_BREATHING
+// #define BACKLIGHT_LEVELS 3
+// #define BREATHING_PERIOD 6
 
 // #define RGB_DI_PIN E2
 // #ifdef RGB_DI_PIN
